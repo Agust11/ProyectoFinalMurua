d6f300dc9dc4b1e5e35c0fca6cf642c826f17a7b
Agust11 <magustinamurua@gmail.com>
2025-08-22 19:29:30 -0300

    Finalizado

--- fichero.js
+++
@@ -1,9 +0,0 @@
-// ficherooo
-function ocultarFichas() {
-  document.querySelectorAll('.ficha').forEach(f => f.classList.add('oculto'));
-}
-
-function mostrarFicha(nombre) {
-  ocultarFichas();
-  document.getElementById(`ficha-${nombre}`).classList.remove('oculto');
-}
